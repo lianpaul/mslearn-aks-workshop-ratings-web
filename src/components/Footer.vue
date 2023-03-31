3ed2c88f212735a835bbaa9f4bce86615cec8cdd
plian57 <bw.lian@gmail.com>
2023-03-31 14:28:56 +0000

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Kubernetes Service (AKS) workshop
+        CIBC Azure Red Hat OpenShift (ARO) - CIBC testing
       </div>
       <div class="col-lg-6">
       </div>
