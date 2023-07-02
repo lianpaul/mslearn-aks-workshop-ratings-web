6f11c7c31da1e6505e78f8761e78b2b12694f6d8
plian57 <bw.lian@gmail.com>
2023-07-02 15:55:56 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        CIBC Azure Red Hat OpenShift (ARO) - CIBC testing
+        CIBC Azure Red Hat OpenShift (ARO) workshop
       </div>
       <div class="col-lg-6">
       </div>
